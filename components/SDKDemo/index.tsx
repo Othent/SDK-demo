570cb5fa9716be023dac682296793424dbb6659c
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-23 00:37:59 -0400

    fix: new API key

--- components/SDKDemo/index.tsx
+++ components/SDKDemo/index.tsx
@@ -16,7 +16,7 @@ const SDKDemo = () => {
   const [othent, setOthent] = useState(null);
   useEffect(() => {
     const initializeOthent = async () => {
-      const othentInstance = await Othent({ API_ID: '1f73e23e3437dd623f5530e90ac1d1b2' });
+      const othentInstance = await Othent({ API_ID: 'd7a29242f7fdede654171a0d3fd25163' });
       setOthent(othentInstance);
     };
     initializeOthent();
